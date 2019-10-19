c52508264d881a0069d9b07b44639f9a57d7a8bf
Felipe Almeida <f.albuquerquedealmeida@gmail.com>
2019-10-19 08:28:33 -0300

    Codigo Inicial

---
+++ WebApplicationAPI/Controllers/ValuesController.cs
@@ -0,0 +1,75 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebApplicationAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private string[] _valuesRepository = new string[1000];       
+
+        public ValuesController(
+            //string[] valuesRepository
+        )
+        {
+            this._valuesRepository[0] = "Felipe Almeida";
+            this._valuesRepository[1] = "Érica Sene";
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return _valuesRepository.Where(prop => prop != null).ToArray();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return _valuesRepository[id];
+        }
+
+        // POST api/values
+        [HttpPost]
+        public ActionResult<string> Post([FromBody] string value)
+        {
+            var position = _valuesRepository.Count(prop => prop != string.Empty
+                                                        && prop != null);
+
+            _valuesRepository[position] = value;
+
+            return value;
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public ActionResult<string> Put(int id, [FromBody] string value)
+        {
+            // transaction
+            // var entity = _valuesRepository[id];
+
+            _valuesRepository[id] = value;
+
+            // save
+
+            return _valuesRepository[id];
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public ActionResult<string> Delete(int id)
+        {
+            // transaction
+            // 
+
+            _valuesRepository[id] = "-";
+
+            // commit
+
+            return "-";
+        }
+    }
+}
---
+++ WebApplicationAPI/Data/AthiaContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplicationAPI.Data
+{
+    using Models;
+
+    public class AthiaContext : DbContext
+    {
+        public AthiaContext (DbContextOptions<AthiaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> User { get; set; }
+    }
+}
